3a2e03579473e010818ac66146c93cc5824bf644
Argus Aphocraphex <zelechos@hotmail.com>
2020-03-26 00:18:20 -0400

    Version 1.5.0

--- PROYECTO AEROPUERTO/src/Clases_Objetos/MAIN.java
+++ PROYECTO AEROPUERTO/src/Clases_Objetos/MAIN.java
@@ -9,7 +9,8 @@ public class MAIN {
     static AEROPUERTOS aeropuertos[] = new AEROPUERTOS[Numero_de_Aeropuerto];
 
     public static void main(String[] args) {
-
+        InsertarDatosAeropuerto(aeropuertos);
+        Menu();
     }
 
     public static void InsertarDatosAeropuerto(AEROPUERTOS Aereo[]) {
@@ -28,11 +29,11 @@ public class MAIN {
         Aereo[0].getCompañias("Aero uss").getVuelos_Actuales("34FER43").Insertar_Pasajero(new PASAJEROS("Ramiro Urquieta Chavez", "475LOA", "Francesa"));
         Aereo[0].getCompañias("Aero uss").getVuelos_Actuales("GRF4522").Insertar_Pasajero(new PASAJEROS("Ramon ludio punina", "M7N6B5", "Italiano"));
         Aereo[0].getCompañias("Aero uss").getVuelos_Actuales("GRF4522").Insertar_Pasajero(new PASAJEROS("ernesto tuto mirto", "L0OP98", "Española"));
-        
-        Aereo[1]= new AEROPUERTOS_PUBLICOS("ARGENTINA AIR", "BUENOS AIRES","ARGENTINA", 99999999);
+
+        Aereo[1] = new AEROPUERTOS_PUBLICOS("ARGENTINA AIR", "BUENOS AIRES", "ARGENTINA", 99999999);
         Aereo[1].Insertar_Compañia(new COMPAÑIAS("Aero Messi"));
         Aereo[1].Insertar_Compañia(new COMPAÑIAS("Higuain mata Penales"));
-        Aereo[1].getCompañias("Aero Messi").Insertar_Vuelo(new VUELOS("290MSE", "BUENOS AIRES", "PARIS", 555.457 , 150));
+        Aereo[1].getCompañias("Aero Messi").Insertar_Vuelo(new VUELOS("290MSE", "BUENOS AIRES", "PARIS", 555.457, 150));
         Aereo[1].getCompañias("Aero Messi").Insertar_Vuelo(new VUELOS("034KEJ", "BUENOS AIRES", "AMSTERDAM", 994.145, 300));
         Aereo[1].getCompañias("Higuain mata Penales").Insertar_Vuelo(new VUELOS("847MSD", "CORDOBA", "BERLIN", 1254.457, 350));
         Aereo[1].getCompañias("Higuain mata Penales").Insertar_Vuelo(new VUELOS("834JCU", "LA PLATA", "DUBAI", 4852.254, 100));
@@ -44,12 +45,11 @@ public class MAIN {
         Aereo[1].getCompañias("Higuain mata Penales").getVuelos_Actuales("834JCU").Insertar_Pasajero(new PASAJEROS("benito quita copas", "FGH022", "Chilena"));
         Aereo[1].getCompañias("Aero Messi").getVuelos_Actuales("034KEJ").Insertar_Pasajero(new PASAJEROS("abel moreno sandoval", "JDSEU3", "Argentina"));
         Aereo[1].getCompañias("Aero Messi").getVuelos_Actuales("034KEJ").Insertar_Pasajero(new PASAJEROS("sergio palacios duran", "2837NJ", "Colombiana"));
-               
-        
-        Aereo[2]= new AEROPUERTOS_PUBLICOS("NEWYORK AIRLINES", "NEW YORK ","E.E.U.U.", 254900999);
+
+        Aereo[2] = new AEROPUERTOS_PUBLICOS("NEWYORK AIRLINES", "NEW YORK ", "E.E.U.U.", 254900999);
         Aereo[2].Insertar_Compañia(new COMPAÑIAS("Boston air"));
         Aereo[2].Insertar_Compañia(new COMPAÑIAS("Texas line"));
-        Aereo[2].getCompañias("Boston air").Insertar_Vuelo(new VUELOS("K8SH43", "NEW YORK", "LOS ANGELES", 548.457 , 170));
+        Aereo[2].getCompañias("Boston air").Insertar_Vuelo(new VUELOS("K8SH43", "NEW YORK", "LOS ANGELES", 548.457, 170));
         Aereo[2].getCompañias("Boston air").Insertar_Vuelo(new VUELOS("NS652V", "BOSTON", "NEW YORK", 349.200, 210));
         Aereo[2].getCompañias("Texas line").Insertar_Vuelo(new VUELOS("M1K33NN", "TEXAS", "WASHITON", 154.784, 102));
         Aereo[2].getCompañias("Texas line").Insertar_Vuelo(new VUELOS("00O2KE", "CHINA", "TEXAS", 982.124, 450));
@@ -61,6 +61,99 @@ public class MAIN {
         Aereo[2].getCompañias("Texas line").getVuelos_Actuales("M1K33NN").Insertar_Pasajero(new PASAJEROS("miguel garzon dieldo", "I8UGHJ", "Chilena"));
         Aereo[2].getCompañias("Texas line").getVuelos_Actuales("00O2KE").Insertar_Pasajero(new PASAJEROS("Edgar Soso betanzos ", "5DSE34", "Argentina"));
         Aereo[2].getCompañias("Texas line").getVuelos_Actuales("00O2KE").Insertar_Pasajero(new PASAJEROS("Pablo Escobar colose", "2NHJ9U", "Colombiana"));
+
+        Aereo[3] = new AEROPUERTOS_PRIVADOS("ABUDAVI AIRLINES", "ABUDAVI ", "EMIRATOS ARABES UNIDOS ");
+        Aereo[3].Insertar_Compañia(new COMPAÑIAS("Air Europa"));
+        String Empresas[] = {"Cobresol", "Aguila34"};
+        ((AEROPUERTOS_PRIVADOS) Aereo[3]).Insertar_Empresas(Empresas);
+        Aereo[3].getCompañias("Air Europa").Insertar_Vuelo(new VUELOS("PPPIII", "NEW YORK", "DUBAI", 9845.457, 90));
+        Aereo[3].getCompañias("Air Europa").getVuelos_Actuales("PPPIII").Insertar_Pasajero(new PASAJEROS("Ford Segundo", "XZXZXZ", "Estado unidense"));
+        Aereo[3].getCompañias("Air Europa").getVuelos_Actuales("PPPIII").Insertar_Pasajero(new PASAJEROS("Bill Gates", "M1CR05", "Estado Unidense"));
+    }
+
+    public static void Menu() {
+        int Opcion;
+        do {
+            System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::MENU::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            System.out.println(":::::            1.- Consultar Aereopuertos Gestionados,(PUBLICOS o PRIVADOS).                                   :::::");
+            System.out.println(":::::            2.- Ver Empresas(PRIVADO) o Subvencion(PUBLICO) .                                               :::::");
+            System.out.println(":::::            3.- Lista de Compañias de un Aereopuerto .                                                      :::::");
+            System.out.println(":::::            4.- Lista de Vuelos Por Compañia .                                                              :::::");
+            System.out.println(":::::            5.- Lista Posibles Vuelos de Origen a Destino .                                                 :::::");
+            System.out.println(":::::            6.- Administrador del Programa .                                                                :::::");
+            System.out.println(":::::            7.- Salir .                                                                                     :::::");
+            System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            System.out.print("Digite una Opcion del Menu : ");
+            Opcion = Entrada.nextInt();
+
+            switch (Opcion) {
+                case 1:
+                    System.out.println("");
+                    MostrarAeropuertos(aeropuertos);
+                    break;
+                case 2:
+                    System.out.println("");
+                    MostrarEmpresasSubvencion(aeropuertos);
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+                    System.out.println("Cerrando Programa . . . ");
+                    break;
+                default:
+                    System.out.println("ERROR DIGITE OTRA OPCION . . . ");
+                    break;
+            }
+
+        } while (Opcion != 7);
+    }
+
+    public static void MostrarAeropuertos(AEROPUERTOS aeropuertos[]) {
+        for (int AeroPuertoACtual = 0; AeroPuertoACtual < aeropuertos.length; AeroPuertoACtual++) {
+            if (aeropuertos[AeroPuertoACtual] instanceof AEROPUERTOS_PRIVADOS) {
+                System.out.println("AEROPUERTOS PRIVADOS : ");
+                System.out.println("Nombre : " + aeropuertos[AeroPuertoACtual].getNombre_de_Aeropuerto());
+                System.out.println("Ciudad : " + aeropuertos[AeroPuertoACtual].getCiudad());
+                System.out.println("Pais : " + aeropuertos[AeroPuertoACtual].getPais());
+            } else {
+                System.out.println("AEROPUERTOS PUBLICOS : ");
+                System.out.println("Nombre : " + aeropuertos[AeroPuertoACtual].getNombre_de_Aeropuerto());
+                System.out.println("Ciudad : " + aeropuertos[AeroPuertoACtual].getCiudad());
+                System.out.println("Pais : " + aeropuertos[AeroPuertoACtual].getPais());
+            }
+            System.out.println("");
+        }
+
+    }
+
+    public static void MostrarEmpresasSubvencion(AEROPUERTOS aeropuertos[]) {
+        String Empresas[];
+        for (int AeroPuertoActual = 0; AeroPuertoActual < aeropuertos.length; AeroPuertoActual++) {
+            if (aeropuertos[AeroPuertoActual] instanceof AEROPUERTOS_PRIVADOS) {
+                System.out.println("Aeropuerto Privado : " + aeropuertos[AeroPuertoActual].getNombre_de_Aeropuerto());
+                Empresas = ((AEROPUERTOS_PRIVADOS) aeropuertos[AeroPuertoActual]).getEmpresas();
+
+                System.out.println("La Empresas del Aeropuerto " + aeropuertos[AeroPuertoActual].getNombre_de_Aeropuerto() + " son : ");
+                for (int EmpresasACtual = 0; EmpresasACtual < Empresas.length; EmpresasACtual++) {
+                    System.out.println("Empresa : " + Empresas[EmpresasACtual]);
+                }
+            } else {
+                System.out.println("La Subvencion de el Aeropuerto : " + aeropuertos[AeroPuertoActual].getNombre_de_Aeropuerto());
+                System.out.println("Subvencion" + ((AEROPUERTOS_PUBLICOS) aeropuertos[AeroPuertoActual]).getSubvencion());
+                
+            }
+            System.out.println("");
+        }
     }
 
 }
