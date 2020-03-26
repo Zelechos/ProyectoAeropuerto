4833928bc44fc89fe6a0a3b596166b49f6f3e602
Tumiri Huanca Alex <zelechos@hotmail.com>
2020-03-25 20:18:19 -0400

    Version 1.0.1

--- PROYECTO AEROPUERTO/src/Clases_Objetos/MAIN.java
+++ PROYECTO AEROPUERTO/src/Clases_Objetos/MAIN.java
@@ -19,10 +19,15 @@ public class MAIN {
         Aereo[0].getCompañias("DigitalFly").Insertar_Vuelo(new VUELOS("QWE231", "TEXAS", "TALAHINA ", 123.23, 100));
         Aereo[0].getCompañias("DigitalFly").Insertar_Vuelo(new VUELOS("S2X3EW", "BUENOS AIRES", "BERLIN", 342.23, 200));
         Aereo[0].getCompañias("Aero uss").Insertar_Vuelo(new VUELOS("34FER43", "PARIS", "UKRANIA", 450.23, 170));
+        Aereo[0].getCompañias("Aero uss").Insertar_Vuelo(new VUELOS("GRF4522", "KIEV", "LIMA", 754.23, 270));
         Aereo[0].getCompañias("DigitalFly").getVuelos_Actuales("QWE231").Insertar_Pasajero(new PASAJEROS("Dembele Peñanieto Volt", "039TER", "Aleman"));
         Aereo[0].getCompañias("DigitalFly").getVuelos_Actuales("QWE231").Insertar_Pasajero(new PASAJEROS("Ortega Miranda ChevsKovsKy", "002UKR", "Ukraniano"));
-        Aereo[0].getCompañias("Aero uss").getVuelos_Actuales("34FER43").Insertar_Pasajero(new PASAJEROS("Esteban", "928KWE", "Argentina"));
+        Aereo[0].getCompañias("DigitalFly").getVuelos_Actuales("S2X3EW").Insertar_Pasajero(new PASAJEROS("morgana ventura ret", "F4T5Y6", "Belga"));
+        Aereo[0].getCompañias("DigitalFly").getVuelos_Actuales("S2X3EW").Insertar_Pasajero(new PASAJEROS("cameron lio york ", "W2E1Q3", "Ucuatoriano"));
+        Aereo[0].getCompañias("Aero uss").getVuelos_Actuales("34FER43").Insertar_Pasajero(new PASAJEROS("Esteban vaca cortez", "928KWE", "Argentina"));
         Aereo[0].getCompañias("Aero uss").getVuelos_Actuales("34FER43").Insertar_Pasajero(new PASAJEROS("Ramiro Urquieta Chavez", "475LOA", "Francesa"));
+        Aereo[0].getCompañias("Aero uss").getVuelos_Actuales("GRF4522").Insertar_Pasajero(new PASAJEROS("Ramon ludio punina", "M7N6B5", "Italiano"));
+        Aereo[0].getCompañias("Aero uss").getVuelos_Actuales("GRF4522").Insertar_Pasajero(new PASAJEROS("ernesto tuto mirto", "L0OP98", "Española"));
         
         Aereo[1]= new AEROPUERTOS_PUBLICOS("ARGENTINA AIR", "BUENOS AIRES","ARGENTINA", 99999999);
         Aereo[1].Insertar_Compañia(new COMPAÑIAS("Aero Messi"));
@@ -40,8 +45,22 @@ public class MAIN {
         Aereo[1].getCompañias("Aero Messi").getVuelos_Actuales("034KEJ").Insertar_Pasajero(new PASAJEROS("abel moreno sandoval", "JDSEU3", "Argentina"));
         Aereo[1].getCompañias("Aero Messi").getVuelos_Actuales("034KEJ").Insertar_Pasajero(new PASAJEROS("sergio palacios duran", "2837NJ", "Colombiana"));
                
-               
         
+        Aereo[2]= new AEROPUERTOS_PUBLICOS("NEWYORK AIRLINES", "NEW YORK ","E.E.U.U.", 254900999);
+        Aereo[2].Insertar_Compañia(new COMPAÑIAS("Boston air"));
+        Aereo[2].Insertar_Compañia(new COMPAÑIAS("Texas line"));
+        Aereo[2].getCompañias("Boston air").Insertar_Vuelo(new VUELOS("K8SH43", "NEW YORK", "LOS ANGELES", 548.457 , 170));
+        Aereo[2].getCompañias("Boston air").Insertar_Vuelo(new VUELOS("NS652V", "BOSTON", "NEW YORK", 349.200, 210));
+        Aereo[2].getCompañias("Texas line").Insertar_Vuelo(new VUELOS("M1K33NN", "TEXAS", "WASHITON", 154.784, 102));
+        Aereo[2].getCompañias("Texas line").Insertar_Vuelo(new VUELOS("00O2KE", "CHINA", "TEXAS", 982.124, 450));
+        Aereo[2].getCompañias("Boston air").getVuelos_Actuales("K8SH43").Insertar_Pasajero(new PASAJEROS("Michel dortman zuckergberg", "CV4B5V", "Rusa"));
+        Aereo[2].getCompañias("Boston air").getVuelos_Actuales("K8SH43").Insertar_Pasajero(new PASAJEROS("Boris sapaski letbxs", "OX87X0", "Ukraniana"));
+        Aereo[2].getCompañias("Boston air").getVuelos_Actuales("NS652V").Insertar_Pasajero(new PASAJEROS("Sergio wiliams esterico", "K8IU7Y", "Española"));
+        Aereo[2].getCompañias("Boston air").getVuelos_Actuales("NS652V").Insertar_Pasajero(new PASAJEROS("hector tuner huzst", "ZSE456", "Boliviana"));
+        Aereo[2].getCompañias("Texas line").getVuelos_Actuales("M1K33NN").Insertar_Pasajero(new PASAJEROS("Victor bravo suarex", "MO4GTH", "Paraguaya"));
+        Aereo[2].getCompañias("Texas line").getVuelos_Actuales("M1K33NN").Insertar_Pasajero(new PASAJEROS("miguel garzon dieldo", "I8UGHJ", "Chilena"));
+        Aereo[2].getCompañias("Texas line").getVuelos_Actuales("00O2KE").Insertar_Pasajero(new PASAJEROS("Edgar Soso betanzos ", "5DSE34", "Argentina"));
+        Aereo[2].getCompañias("Texas line").getVuelos_Actuales("00O2KE").Insertar_Pasajero(new PASAJEROS("Pablo Escobar colose", "2NHJ9U", "Colombiana"));
     }
 
 }
